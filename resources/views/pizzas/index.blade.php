488e7ac2be32ed6478de232bbf1e57dfeb3ed432
NicholasDevelop <marcaccini.n@gmail.com>
2022-04-29 13:07:02 +0200

    Complete scaffolding

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,24 +1,30 @@
-<div class="container">
-  <table>
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>nome</th>
-        <th>ingredienti</th>
-        <th>prezzo</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($pizzas as $pizza)
+@extends('layout.layout')
+
+
+@section('section')
+  <div class="container">
+    <table>
+      <thead>
         <tr>
-          <td> {{$pizza->id}} </td>
-          <td> {{$pizza->name}} </td>
-          <td> {{$pizza->description}} </td>
-          <td> {{$pizza->price}} &euro;</td>
-          <td></td>
+          <th>id</th>
+          <th>nome</th>
+          <th>ingredienti</th>
+          <th>prezzo</th>
+          <th></th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
-</div>
+      </thead>
+      <tbody>
+        @foreach($pizzas as $pizza)
+          <tr>
+            <td> {{$pizza->id}} </td>
+            <td> {{$pizza->name}} </td>
+            <td> {{$pizza->description}} </td>
+            <td> {{$pizza->price}} &euro;</td>
+            <td></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+@endsection
